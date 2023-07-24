e08700e82571c1edecb5cf2802554cd0b3b37771
Melis Ibisheva <93177517+MelisIbisheva@users.noreply.github.com>
2023-07-24 15:11:38 +0300

    working with abstraction

---
+++ softini-java-oop/WorkingWithAbstraction_lab/HotelReservation/Discount.java
@@ -0,0 +1,33 @@
+package WorkingWithAbstraction_lab.HotelReservation;
+
+import javax.print.DocFlavor;
+
+public enum Discount {
+    VIP(0.80),
+    SECOND_VISIT(0.90),
+    NONE(1);
+
+    private double priceReduction;
+
+    Discount(double priceReduction){
+        this.priceReduction = priceReduction;
+    }
+
+    public static Discount parse(String str) {
+        switch (str){
+            case "VIP":
+                return VIP;
+            case "SecondVisit":
+                return SECOND_VISIT;
+            case "None":
+                return NONE;
+            default:
+                throw new IllegalArgumentException("Unknown enum value" + str);
+
+        }
+    }
+
+    public double getPriceReduction() {
+        return priceReduction;
+    }
+}
